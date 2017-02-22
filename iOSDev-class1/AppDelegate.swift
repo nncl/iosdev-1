0b59b3c05d7c75390c4f3a2251680ea18c304ee8
Usuário Convidado <Guest@PA06MICRO16.local>
2017-02-22 20:19:52 -0300

    Buttons

--- iOSDev-class1/AppDelegate.swift
+++ iOSDev-class1/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Set all Tint colors to the same color
+        window?.tintColor = .red
+        
         return true
     }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 - New StoryBoard - Label
 - New View Controller
 
+- Tint color
+	- multiples at the same time, not in the story board one by one
+
